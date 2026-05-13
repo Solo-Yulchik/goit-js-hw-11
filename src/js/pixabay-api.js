d317ca88d7e9b77c90d8c1e546e29607d0f3db39
yuliia-surova <surovuiyulchik23@gmail.com>
2026-05-13 17:09:59 +0200

    Modul-11-4

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -9,9 +9,9 @@ export function getImagesByQuery(query){
         q:query,
         image_type:"photo",
         orientation:"horizontal",
-        safesearch:"true"
+        safesearch:true
     }})
     .then(({data})=>data)
-console.log(data);
+
 
 }
